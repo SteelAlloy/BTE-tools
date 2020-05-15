7918445648d9c96020231067d7a2c276221de1e6
Nathael NOGUES <nathael.nogues@gmail.com>
2020-05-15 15:32:13 +0200

    Using threads for requesting IGN data

--- src/ign.js
+++ src/ign.js
@@ -7,6 +7,7 @@ importPackage(Packages.com.sk89q.worldedit.math)
 importPackage(Packages.com.sk89q.worldedit.blocks)
 importPackage(Packages.java.io)
 importPackage(Packages.java.net)
+importPackage(Packages.java.lang)
 importPackage(Packages.java.nio.charset)
 importPackage(Packages.org.apache.commons.io)
 importPackage(Packages.javax.net.ssl)
@@ -34,15 +35,22 @@ const region = session.getRegionSelector(player.getWorld()).getRegion()
 
 const air = context.getBlock('air')
 const water = context.getBlock('water')
+const lava = context.getBlock('lava')
+
+const vectorUp = new Vector(0, 1, 0)
+const vectorDown = new Vector(0, -1, 0)
 
 if (!options.water) {
-  ignoredBlocks = ignoredBlocks.concat([
-    context.getBlock('water').id,
-    context.getBlock('lava').id
-  ])
+  if (ignoredBlocks.indexOf(water.id) < 0) {
+    ignoredBlocks.push(water.id)
+  }
+  if (ignoredBlocks.indexOf(lava.id) < 0) {
+    ignoredBlocks.push(lava.id)
+  }
 }
 
-const { coords, geoCoords } = getRegion()
+// Run
+const selectedCoords = getRegion()
 
 try {
   ign()
@@ -50,50 +58,102 @@ try {
   player.printError((err.message + '').split('http')[0])
 }
 
-function getRegion () {
+// functions
+
+function getRegion() {
   player.print('§7Please wait...')
   const projection = getProjection()
 
-  const coords = [[]]
-  const geoCoords = [[]]
-  let index = 0
-  let length = 0
-
+  const coords = []
   const minX = region.getMinimumPoint().getX()
   const minZ = region.getMinimumPoint().getZ()
-  for (let x = minX; x < region.getWidth() + minX; x++) {
-    for (let z = minZ; z < region.getLength() + minZ; z++) {
-      if (length >= 150) {
-        coords.push([])
-        geoCoords.push([])
-        index++
-        length = 0
-      }
-      length++
-      coords[index].push([x, z])
-      geoCoords[index].push(projection.toGeo(x, z))
+  const maxX = region.getWidth() + minX
+  const maxZ = region.getLength() + minZ
+  for (let x = minX; x < maxX; x++) {
+    for (let z = minZ; z < maxZ; z++) {
+      coords.push({ x: x, z: z, geo: projection.toGeo(x, z) })
     }
   }
 
-  return { coords, geoCoords }
+  return coords
 }
 
-function ign () {
-  for (let i = 0; i < geoCoords.length; i++) {
-    const query = `http://wxs.ign.fr/choisirgeoportail/alti/rest/elevation.json?lon=${geoCoords[i].map((block) => block[0]).join('|')}&lat=${geoCoords[i].map((block) => block[1]).join('|')}&zonly=true`
-    const elevations = JSON.parse(request(query)).elevations
-    if (elevationError(elevations)) {
-      player.printError(areaError)
-    } else {
-      for (let j = 0; j < elevations.length; j++) {
-        const [x, z] = coords[i][j]
-        elevateGround(new Vector(x, Math.floor(elevations[j] + 0.5), z))
-      }
+function getLon(coord) {
+  return coord.geo[0]
+}
+function getLat(coord) {
+  return coord.geo[1]
+}
+
+function ign() {
+  let retries = [] // stock coords that failed once, to retry fetching them once after
+
+  let pendingRequests = 0
+  let onRetryNeeded = () => {
+    player.print(`${retries.length} blocs have failed to elevate. Retrying...`)
+
+    // extract retries list
+    const retrying = retries
+
+    // empty retires list
+    retires = []
+
+    // do not retry once more, show error message instead infinite loop
+    onRetryNeeded = () => {
+      player.printError(`${retries.length} blocs failed to elevate.\nPlease select a slightly different region.`)
+    }
+
+    // retry once with smaller groups
+    runReqs(retrying, 50)
+  }
+  const onReqError = (error) => {
+    player.print('reqError')
+    retries = retries.concat(group)
+  }
+
+  const elevationMap = [];
+
+  const runReqs = (allCoords, maxSimultaneous) => {
+    const allThreads = []
+    for (let i = 0; i < allCoords.length; i += maxSimultaneous) {
+      const group = allCoords.slice(i, i + maxSimultaneous)
+      const lons = group.map(getLon).join('|')
+      const lats = group.map(getLat).join('|')
+      const query = `http://wxs.ign.fr/choisirgeoportail/alti/rest/elevation.json?lon=${lons}&lat=${lats}&zonly=true`
+
+      allThreads.push(requestAsync(query, (data) => {
+        const elevations = data.elevations
+        for (let j = 0; j < elevations.length; j++) {
+          if (elevations[j] > -99999) {
+            elevationMap.push({ x: group[j].x, y: (elevations[j] + .5) | 0, z: group[j].z })
+          } else {
+            retries.push(group[j]);
+          }
+        }
+      }, onReqError))
+    }
+    player.print(`Requesting information (${allCoords.length} blocs divided into ${allThreads.length} requests)...`)
+
+    for (let i = 0; i < allThreads.length; i++) {
+      allThreads[i].join()
+    }
+
+    if (retries.length > 0) {
+      onRetryNeeded();
     }
   }
+
+  runReqs(selectedCoords, 150)
+
+  player.print('Elevating...')
+  for (let j = 0; j < elevationMap.length; j++) {
+    elevateGround(new Vector(elevationMap[j].x, elevationMap[j].y, elevationMap[j].z))
+  }
+  player.print('End of IGN: ' + elevationMap.length + '/' + selectedCoords.length)
 }
 
-function elevateGround (pos) {
+function elevateGround(pos) {
+  // look for current ground location
   let ground = pos
   while (!ignoredBlocks.includes(blocks.getBlock(ground.add(new Vector(0, 1, 0))).id)) {
     ground = ground.add(new Vector(0, 1, 0))
@@ -101,6 +161,8 @@ function elevateGround (pos) {
   while (ignoredBlocks.includes(blocks.getBlock(ground).id)) {
     ground = ground.add(new Vector(0, -1, 0))
   }
+
+  // update ground height
   const block0 = blocks.getBlock(ground)
   const block1 = blocks.getBlock(ground.add(new Vector(0, -1, 0)))
   if (ground.y < pos.y) {
@@ -119,17 +181,19 @@ function elevateGround (pos) {
   }
 }
 
-function elevationError (elevations) {
-  return elevations.includes(-99999.0)
-}
-
-function request (urlText) {
-  const url = new URL(urlText)
-  const c = url.openConnection()
-
-  const is = c.getInputStream()
-
-  const writer = new StringWriter()
-  IOUtils.copy(is, writer, StandardCharsets.UTF_8)
-  return writer.toString()
+function requestAsync(url, onSuccess, onError) {
+  /**
+   * url: string like "http://xxx.com/..."
+   * cb: function like cb(data, error)
+   *    data: data returned by the url; null in case of error
+   *    error: null if data returned; error description in case of problem
+   */
+  const t = new Thread(() => {
+    const c = new URL(url).openConnection()
+    const writer = new StringWriter()
+    IOUtils.copy(c.getInputStream(), writer, StandardCharsets.UTF_8)
+    onSuccess(JSON.parse(writer.toString()))
+  })
+  t.start()
+  return t
 }
